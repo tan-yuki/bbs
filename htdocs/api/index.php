b3bbf3cd2b4942b7dfbacf38260f282c027da237
tanakayuki <a0673040@gmail.com>
2013-02-07 18:03:33 +0900

    add mock API

--- htdocs/api/index.php
+++ htdocs/api/index.php
@@ -3,56 +3,110 @@
 $appRoot = '../../';
 require_once $appRoot . 'vendor/autoload.php';
 require_once './SlimWrapper.php';
-require_once './TaskApi.php';
+require_once './BBSApi.php';
 
 // application instance
-$todo = new SlimWrapper($appRoot . "todo.ini");
-$app = $todo->getApplication();
+$bbs = new SlimWrapper($appRoot . "bbs.ini");
+$app = $bbs->getApplication();
 
 
-// ====== define routing =====
 
-// Register to-do task API
-//
-// METHOD: PUT
-// @param user_id      (required)
-// @param description  (required)
-// @param title        (required)
-$app->put('/task/save/', function () use ($todo) {
-	$app = $todo->getApplication();
-	$req = $app->request();
-
-	$errmsgs = TaskApi::save($req->params());
-	if (empty($errmsgs)) {
-		$todo->success();
-		return;
-	}
-
-	$todo->fail($errmsgs);
-	return;
+$app->get('/bbs/post/', function () use ($bbs) {
+//    $app = $bbs->getApplication();
+//    $req = $app->request();
+//    $tasks = TaskApi::getPostsByThreadId($thread_id);
+//    $bbs->success($tasks);
+	$bbs->success(array(
+		array(
+			"id"   => 1,
+			"title" => 'post1',
+			"contents" => 'This is post1',
+			"update_date" => "2013-02-07 10:00:00",
+			"create_date" => "2013-02-07 10:00:00"
+		),
+		array(
+			"id"   => 2,
+			"title" => 'post2',
+			"contents" => 'This is post1',
+			"update_date" => "2013-02-07 10:00:00",
+			"create_date" => "2013-02-07 10:00:00"
+		),
+		array(
+			"id"   => 3,
+			"title" => 'post3',
+			"contents" => 'This is post3',
+			"update_date" => "2013-02-07 10:00:00",
+			"create_date" => "2013-02-07 10:00:00"
+		),
+		array(
+			"id"   => 4,
+			"title" => 'post4',
+			"contents" => 'This is post4',
+			"update_date" => "2013-02-07 10:00:00",
+			"create_date" => "2013-02-07 10:00:00"
+		),
+		array(
+			"id"   => 5,
+			"title" => 'post5',
+			"contents" => 'This is post5',
+			"update_date" => "2013-02-07 10:00:00",
+			"create_date" => "2013-02-07 10:00:00"
+		),
+	));
 });
 
-// Get to-do task API
-//
-// METHOD: POST
-// @param user_id      (required)
-// @param description  (required)
-// @param title        (required)
-$app->get('/task/:user_id/', function ($user_id) use ($todo) {
-	$app = $todo->getApplication();
-
-	if (empty($user_id)) {
-		$todo->fail("Empty user_id");
-		return;
-	}
-
-	$req = $app->request();
-	$tasks = TaskApi::get($user_id);
-	$todo->success($tasks);
+$app->get('/bbs/thread/', function () use ($bbs) {
+//    $app = $bbs->getApplication();
+//    $req = $app->request();
+//    $threads = TaskApi::getThreads();
+//    $bbs->success($threads);
+	$bbs->success(array(
+		array(
+			"id"   => 1,
+			"name" => 'thread1'
+		),
+		array(
+			"id"   => 2,
+			"name" => 'thread2'
+		),
+		array(
+			"id"   => 3,
+			"name" => 'thread3'
+		),
+		array(
+			"id"   => 4,
+			"name" => 'thread4'
+		),
+		array(
+			"id"   => 5,
+			"name" => 'thread5'
+		),
+	));
 });
 
-$app->notFound(function() use ($todo) {
-	$todo->fail("Wrong url path");
+$app->get('/bbs/category/', function () use ($bbs) {
+//    $app = $bbs->getApplication();
+//    $req = $app->request();
+//    $categories = TaskApi::getCategories();
+//    $bbs->success($categories);
+	$bbs->success(array(
+		array(
+			"id"   => 1,
+			"name" => 'カテゴリー1'
+		),
+		array(
+			"id"   => 2,
+			"name" => 'カテゴリー2'
+		),
+		array(
+			"id"   => 3,
+			"name" => 'カテゴリー3'
+		),
+	));
+});
+
+$app->notFound(function() use ($bbs) {
+	$bbs->fail("Wrong url path");
 	return;
 });
 
